df35e00cbc7c28ad4008c7011ebe0a930a3d2f47
davelowqx <67348776+davelowqx@users.noreply.github.com>
2021-09-19 06:00:25 +0800

    Add padding for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Layout from "components/layout";
 import Seo from "components/Seo";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 11rem 0 8rem;
+  text-align: center;
+`;
 
 const NotFound = () => (
   <Layout>
     <Seo title="404: Not found" location="/404" />
-    <div className="center">
+    <Container>
       <h1>NOT FOUND</h1>
       <p>
         Nothing to see here... <a href="/">back</a>
       </p>
-    </div>
+    </Container>
   </Layout>
 );
 
